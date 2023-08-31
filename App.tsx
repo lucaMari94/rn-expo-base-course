4031d8c57fe28f45bb2d3d6a75938378739f0049
Luca Marignati <luca.marignati@larus-ba.it>
2023-08-31 15:32:27 +0200

    fix

--- App.tsx
+++ App.tsx
@@ -2,12 +2,19 @@ import React from "react";
 import { StyleSheet, Text, Button, View } from "react-native";
 
 export default function App() {
+  /* Adding style:
+   * margin: 16 pixel
+   * padding: 16
+   * borderColor: "red"
+   * borderWidth: 1
+   * borderStyle: "solid"
+   * */
   return (
     <View style={styles.container}>
       <View>
         <Text>Another piece of text!</Text>
       </View>
-      <Text>Hello world!</Text>
+      <Text> Hello world!</Text>
       <Button title="Tap me!" />
     </View>
   );
