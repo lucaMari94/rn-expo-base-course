eb918526708a01e19602c44fa6dc2b0e833b976a
Mattia Zaratin <mattiazaratin@mzaratin-2.fritz.box>
2023-09-06 15:44:43 +0200

    Fixes

--- App.tsx
+++ App.tsx
@@ -1,11 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Button, ScrollView, StyleSheet, Text, View } from "react-native";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+const baseUrl = "https://reqres.in";
 
 export default function App() {
+  const [newValue, setNewValue] = useState<Array<any>>([]);
+
+  const handlePostValue = () => {
+    axios({
+      method: "post",
+      url: `${baseUrl}/api/users`,
+      data: {
+        name: "morpheus",
+        job: "leader",
+      },
+    }).then((response) => {
+      const tmp = [...newValue];
+      tmp.push(response.data);
+      setNewValue(tmp);
+    });
+  };
+
+  useEffect(() => {
+    axios({
+      method: "get",
+      url: `${baseUrl}/api/users`,
+    }).then((response) => {
+      setNewValue(response.data.data);
+    });
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <View>
+        <Button onPress={() => handlePostValue()} title="Post call"></Button>
+      </View>
+      <ScrollView>
+        {newValue?.map((t) => (
+          <View>
+            <Text>{JSON.stringify(t)}</Text>
+          </View>
+        ))}
+      </ScrollView>
     </View>
   );
 }
@@ -13,8 +53,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
